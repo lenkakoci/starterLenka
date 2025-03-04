6b986db5d6df73ecf64f3ab343dbf7e663b0ecdf
lenka.kocianova <lenka.koc@seznam.cz>
2025-03-04 14:15:15 +0100

    odstraněny nepoužité komponenty

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import React from "react";
 // import { Event, EventProps } from "./components/Event/Event";
 import { Events } from "./pages/Events/Events";
 import { EventDetail } from "./pages/EventDetail/EventDetail";
-import { UncontrolledForm } from "./pages/UncontrolledForm/UncontrolledForm";
-import { ControlledForm } from "./pages/ControlledForm/ControlledForm";
 import { NovaAkceFormik } from "./pages/NovaAkceFormik/NovaAkceFormik";
 // import { Navigation } from "./components/Navigation/Navigation";
 import { Route, BrowserRouter, Routes } from "react-router";
@@ -21,8 +19,6 @@ const App: React.FC = () => {
           <Route path="/events" element={<Events data={data} />} />
           <Route path="/events/:id" element={<EventDetail />} />
           <Route path="/formik" element={<NovaAkceFormik />} />
-          <Route path="/controlled" element={<ControlledForm />} />
-          <Route path="/uncontrolled" element={<UncontrolledForm />} />
         </Routes>
       </BrowserRouter>
     </div>
